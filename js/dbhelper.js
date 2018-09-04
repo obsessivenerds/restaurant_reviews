9b987fb10c7c887aa730827fc6a0bd6e28fd6f2a
Nate Wooten <njwooten@outlook.com>
2018-09-03 21:49:38 -0400

    added aria labels

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -157,7 +157,7 @@ class DBHelper {
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
       alt: restaurant.name,
@@ -165,7 +165,7 @@ class DBHelper {
       })
       marker.addTo(newMap);
     return marker;
-  } 
+  }
   /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
@@ -179,3 +179,14 @@ class DBHelper {
 
 }
 
+//if ('serviceWorker' in navigator) {
+/*     window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service_worker.js').then(function(registration) {
+            // Registration was successful
+            console.log('The service worker logged the scope:', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('I failed. Here is what went wrong: ', err);
+        });
+    });
+}*/
---
+++ service_worker.js
@@ -0,0 +1,41 @@
+/*var MY_CACHE = 'restaurant-app-s1';
+var urlsToCache = ['/', 'index.html', 'restaurant.html', '/css/styles.css', '/js/dbhelper.js', '/js/restaurant_info.js', '/js/main.js', 'data/restaurants.json', 'https://fonts.googleapis.com/css?family=Lato:300,400', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js'];
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(caches.open(MY_CACHE).then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+    }));
+});
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(caches.match(event.request).then(function(response) {
+        // Cache hit - return response
+        if (response) {
+            return response;
+        }
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(function(response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(MY_CACHE).then(function(cache) {
+                cache.put(event.request, responseToCache);
+            });
+            return response;
+        });
+    }));
+});
+self.addEventListener('activate', function(event) {
+    var cacheList = ['offline'];
+    event.waitUntil(caches.keys().then(function(cacheNames) {
+        return Promise.all(cacheNames.map(function(cacheName) {
+            if (cacheList.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+            }
+        }));
+    }));
+});
+*/
